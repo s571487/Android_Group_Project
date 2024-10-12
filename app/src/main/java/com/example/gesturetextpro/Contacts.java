ed99c446f3db8d9c223076d0be08bf2c5917ef94
S571495 <ys.sara2068@gmail.com>
2024-10-11 22:36:19 -0500

    Updated Contacts activity

--- app/src/main/java/com/example/gesturetextpro/Contacts.java
+++ app/src/main/java/com/example/gesturetextpro/Contacts.java
@@ -1,24 +1,20 @@
 package com.example.gesturetextpro;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Contacts extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contacts);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void gesture(View view) {
+        Intent intent = new Intent(Contacts.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
