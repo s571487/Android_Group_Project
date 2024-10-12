ac4ae931425d57df0c9f90a02957b841528568d0
saicharanreddysuravarapu <charanreddysuravarapu@gmail.com>
2024-10-11 23:29:37 -0500

    Created GroupMessaging Activity

--- app/src/main/java/com/example/gesturetextpro/MainActivity.java
+++ app/src/main/java/com/example/gesturetextpro/MainActivity.java
@@ -36,4 +36,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this,Settings.class);
         startActivity(intent);
     }
+
+    public void GroupMessaging(View view) {
+        Intent intent = new Intent(MainActivity.this,GroupMessaging.class);
+        startActivity(intent);
+    }
 }
