eade5655788298c6cf3cb330bc4e1789547faa06
noorbashaasha <noorbashaasha2017@gmail.com>
2024-11-22 14:00:20 -0600

    [S572176-Asha] Added ChatAdapter.java

---
+++ app/src/main/java/com/example/gesturetextpro/adapters/ChatAdapter.java
@@ -0,0 +1,63 @@
+package com.example.gesturetextpro.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.gesturetextpro.R;
+import com.example.gesturetextpro.models.Message;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
+    private List<Message> messages;
+    private String currentUserId;
+
+    public ChatAdapter(List<Message> messages, String currentUserId) {
+        this.messages = messages;
+        this.currentUserId = currentUserId;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int layoutRes = viewType == 0 ? R.layout.item_message_sent : R.layout.item_message_received;
+        View view = LayoutInflater.from(parent.getContext()).inflate(layoutRes, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messages.get(position);
+        holder.messageText.setText(message.getContent());
+
+        if (holder.timestampText != null) {
+            holder.timestampText.setText(message.getFormattedTimestamp());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return messages.get(position).getSenderId().equals(currentUserId) ? 0 : 1;
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView timestampText;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+        }
+    }
+
+    public void updateMessages(List<Message> newMessages) {
+        this.messages = newMessages;
+        notifyDataSetChanged();
+    }
+}
